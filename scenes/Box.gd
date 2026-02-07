23af343fcf6d25478afbae0ecdbe5c959704690a
Anomasie <umdieeckedenker@gmail.com>
2026-02-07 18:26:56 +0100

    added ok position editing

--- scenes/Box.gd
+++ scenes/Box.gd
@@ -1,4 +1,7 @@
 extends CSGBox3D
+class_name ResizableBox
+
+signal focus_me
 
 @onready var BoxArea = $BoxArea
 
@@ -7,7 +10,9 @@ var focused = false
 var original_scale = self.scale
 var original_position = Vector3.ZERO
 var editing_face = Vector3.ZERO
+var editing_position = false
 var drag_center = Vector3.ZERO # point on which the face / edge was clicked
+var drag_offset = Vector3.ZERO # point inside the InnerArea that was clicked
 
 @onready var FaceAreas = [
 	$Area100, $Area200,
@@ -15,8 +20,11 @@ var drag_center = Vector3.ZERO # point on which the face / edge was clicked
 	$Area001, $Area002
 ]
 
+func set_focus(value = true):
+	if value: focus()
+	else: defocus()
+
 func focus():
-	print("focusing!")
 	focused = true
 	self.material.albedo_color.a = 0.5
 	BoxArea.hide()
@@ -51,11 +59,26 @@ func _process(_delta):
 		self.position = original_position + difference/2 * editing_face
 		
 		if not Input.is_action_pressed("click"):
-			editing_face = false
+			editing_face = Vector3.ZERO
+	elif editing_position:
+		var camera_position = get_viewport().get_camera_3d().global_transform.origin
+		var camera_direction = get_viewport().get_camera_3d().project_ray_normal(
+			get_viewport().get_mouse_position()
+		)
+		
+		var v = camera_position
+		var x = drag_center
+		var d = camera_direction
+		var s = v.dot(x-v)/v.dot(d)
+		
+		self.position = camera_position + s * d - drag_offset
+		
+		if not Input.is_action_pressed("click"):
+			editing_position = false
 
 func _on_box_area_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
-	if event.is_action_pressed("click"):
-		focus()
+	if event.is_action_released("click"):
+		focus_me.emit(self)
 
 func _on_area_100_input_event(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
@@ -78,7 +101,7 @@ func _on_area_010_input_event(_camera: Node, event: InputEvent, event_position: 
 		original_position = self.position
 		editing_face = Vector3(0,1,0)
 
-func _on_area_020_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _on_area_020_input_event(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
 		drag_center = event_position
 		original_scale = self.scale
@@ -92,9 +115,16 @@ func _on_area_001_input_event(_camera: Node, event: InputEvent, event_position: 
 		original_position = self.position
 		editing_face = Vector3(0,0,1)
 
-func _on_area_002_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _on_area_002_input_event(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
 		drag_center = event_position
 		original_scale = self.scale
 		original_position = self.position
 		editing_face = Vector3(0,0,-1)
+
+func _on_inner_area_input_event(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event.is_action_pressed("click"):
+		drag_center = event_position
+		drag_offset = event_position - self.position
+		original_position = self.position
+		editing_position = true
--- scenes/playground/Playground.gd
+++ scenes/playground/Playground.gd
@@ -1,1 +1,15 @@
 extends Node3D
+
+@onready var Boxes = $Boxes.get_children()
+
+func _ready():
+	for i in len(Boxes):
+		Boxes[i].focus_me.connect(_on_box_focus_me)
+
+func focus(MyRects):
+	for child in Boxes:
+		if child is ResizableBox:
+			child.set_focus( child in MyRects )
+
+func _on_box_focus_me(box):
+	focus([box])

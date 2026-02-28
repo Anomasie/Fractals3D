06fbada439941daa5685da9a843ba08ab64c33ac
Anomasie <umdieeckedenker@gmail.com>
2026-02-28 13:04:50 +0100

    ?

--- file-contact/MyFileDialog.gd
+++ file-contact/MyFileDialog.gd
@@ -8,6 +8,7 @@ func open():
 	popup()
 	invalidate() # it was supposed to help fixing a bug, but I am not sure what it does
 	# but everyone seems to recommend it, so I guess I'll leave it in
+	
 
 func close():
 	hide()
